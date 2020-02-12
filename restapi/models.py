94b294d0038baa506b288d42567b759f1f9f547f
funawo <funawo@example.com>
2020-02-13 06:51:29 +0900

    add qoupon table

--- restapi/models.py
+++ restapi/models.py
@@ -8,3 +8,14 @@ class Question(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=30)
+
+# Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=20)
+    benefit = models.CharField(max_length=1000)
+    explanation = models.CharField(max_length=2000)
+    image = models.CharField(max_length=500, null=True, blank=True) # 画像URL格納用に追加
+    store = models.CharField(max_length=1000)
+    start = models.DateField()
+    deadline = models.DateField()
+    status = models.BooleanField()
